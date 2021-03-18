b3d2e36322116afa0c0b5e91a46a5928a12d8881
Manuel Alejandro Bemudez <mabermudez87952@umanizales.edu.co>
2021-03-18 17:08:15 -0500

    Métodos de la clase main mas organizados
    Falta hacer que "elecc" se comporte como "Persona" en vez de como String

--- src/laboratoriogit/LaboratorioGit.java
+++ src/laboratoriogit/LaboratorioGit.java
@@ -16,26 +16,37 @@ import modelo.Persona;
  * @author MANUEL BERMUDEZ
  */
 public class LaboratorioGit {
+    List<Persona> Personas = new ArrayList<>();
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        List<Persona> Personas = new ArrayList<>();
         
         Scanner  entrada = new Scanner(System.in); //crea el scanner para ingresar datos por teclado
         String elecc;
         System.out.println("Desea ingresar mas Personas?");
         elecc=entrada.nextLine();
-        while("si".equals(elecc)){
-            
-            String elecc2;
-            System.out.print("Ingrese su rol");
-            elecc2=entrada.nextLine();
-        }
+        
+        
         
     }
     
+    
+    public void eleccion(String elecc){
+       while("si".equals(elecc)){
+            Scanner  entrada = new Scanner(System.in);
+            String elecc2;
+            System.out.print("Ingrese su rol");
+            elecc2=entrada.nextLine();
+            
+            
+            /* HACER QUE PERSONA SE COMPORTE COMO "PERSONA" EN VEZ DE STRING
+            Personas.add((Persona)elecc2);
+        
+*/
+    }
+    }
     
 }
